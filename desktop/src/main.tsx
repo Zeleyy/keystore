819dab3c3a7add83c3f5656380d546c8d6e423a1
Zeleyy <afuylal@gmail.com>
2026-06-30 18:03:25 +0300

    feat(desktop): [#3] setup fsd architecture, routing, and tanstack query client

--- desktop/src/main.tsx
+++ desktop/src/main.tsx
@@ -1,10 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import "./app/styles/index.scss";
+import { createRoot } from "react-dom/client";
+import { QueryProvider } from "./app/providers";
+import App from "./app/App.tsx";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+createRoot(document.getElementById("root")!).render(
+    <QueryProvider>
+        <App />
+    </QueryProvider>
+);
